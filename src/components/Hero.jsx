6a4c8b593510d4eafd30bb1554dcf6135ea3b8d9
siwarAbdelhedi <abdelhedi.siwar.it@gmail.com>
2025-02-02 14:24:03 +0100

    ✨ Fix alignement du Hero section & amélioration responsive �

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Button, Grid } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Athlete from "../assets/home.png";
 
@@ -7,44 +7,49 @@ const HeroContainer = styled(Box)(() => ({
   minHeight: "100vh",
   display: "flex",
   alignItems: "center",
-  width: "100vw",
-  position: "relative",
-  marginLeft: "calc(-50vw + 50%)",
-  marginRight: "calc(-50vw + 50%)",
+  justifyContent: "center",
+  width: "100%",
   overflow: "hidden",
 }));
 
-const TextContainer = styled(Box)(({ theme }) => ({
-  textAlign: "left",
-  maxWidth: "700px",
-  zIndex: 1,
-  [theme.breakpoints.down("md")]: {
+// Limite la largeur pour bien gérer les écrans larges
+const HeroContent = styled(Box)(() => ({
+  width: "100%",
+  maxWidth: "1600px",
+  padding: "0 80px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  flexDirection: "row",
+  "@media (max-width: 960px)": {
+    flexDirection: "column",
+    padding: "40px 20px",
     textAlign: "center",
-    maxWidth: "100%",
-    padding: "0 20px",
   },
 }));
 
-const HeroImage = styled("img")(({ theme }) => ({
+// Texte bien aligné à gauche
+const TextContainer = styled(Box)(() => ({
+  maxWidth: "600px",
+  "@media (max-width: 960px)": {
+    textAlign: "center",
+  },
+}));
+
+// Image alignée à droite
+const HeroImage = styled("img")(() => ({
   width: "auto",
-  height: "70vh",
-  display: "block",
-  marginLeft: "auto",
-  position: "relative",
-  [theme.breakpoints.down("md")]: {
+  maxWidth: "500px",
+  height: "80vh",
+  objectFit: "contain",
+  "@media (max-width: 960px)": {
     height: "auto",
-    width: "80%",
-    maxWidth: "400px",
-    margin: "2rem auto 0",
-    position: "relative",
-    right: "0",
-  },
-  [theme.breakpoints.down("sm")]: {
-    width: "70%",
-    maxWidth: "300px",
+    maxWidth: "80%",
+    marginTop: "20px",
   },
 }));
 
+// Bouton stylisé
 const StyledButton = styled(Button)(() => ({
   backgroundColor: "white",
   color: "#232A45",
@@ -62,82 +67,27 @@ const StyledButton = styled(Button)(() => ({
 const Hero = () => {
   return (
     <HeroContainer>
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: "1440px",
-          margin: "0 auto",
-          padding: {
-            xs: "20px",
-            md: "40px",
-          },
-        }}
-      >
-        <Grid
-          container
-          alignItems="center"
-          spacing={0}
-          direction={{ xs: "column", md: "row" }}
-        >
-          {/* Text section */}
-          <Grid
-            item
-            xs={12}
-            md={6}
+      <HeroContent>
+        {/* ✅ Texte à gauche */}
+        <TextContainer>
+          <Typography
+            variant="h2"
             sx={{
-              order: { xs: 2, md: 1 },
-              mt: { xs: 4, md: 0 },
+              fontWeight: "bold",
+              color: "#14235E",
+              fontFamily: "Decalotype, sans-serif",
+              fontSize: { xs: "1.75rem", sm: "2.5rem", md: "3rem" },
             }}
           >
-            <TextContainer>
-              <Typography
-                variant="h2"
-                sx={{
-                  fontWeight: "bold",
-                  color: "#232A45",
-                  fontFamily: "Decalotype, sans-serif",
-                  fontSize: {
-                    xs: "1.75rem",
-                    sm: "2rem",
-                    md: "3rem",
-                  },
-                  marginLeft: { xs: 0, md: 10 },
-                  textAlign: { xs: "center", md: "left" },
-                }}
-              >
-                Votre marque de vêtements de sport <br />
-                <span style={{ color: "#232A45" }}>100% adaptable</span>
-              </Typography>
-              <StyledButton
-                sx={{
-                  mt: 3,
-                  marginLeft: { xs: 0, md: 10 },
-                  display: "block",
-                  margin: { xs: "20px auto", md: "24px 0 0 80px" },
-                }}
-              >
-                Qui sommes nous ?
-              </StyledButton>
-            </TextContainer>
-          </Grid>
+            Votre marque de vêtements de sport <br />
+            <span style={{ color: "#14235E" }}>100% adaptable</span>
+          </Typography>
+          <StyledButton sx={{ mt: 3 }}>Qui sommes nous ?</StyledButton>
+        </TextContainer>
 
-          {/* Image section */}
-          <Grid
-            item
-            xs={12}
-            md={6}
-            sx={{
-              order: { xs: 1, md: 2 },
-              position: { md: "relative" },
-              right: { md: "-23px" },
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <HeroImage src={Athlete} alt="Athlète en fauteuil roulant" />
-          </Grid>
-        </Grid>
-      </Box>
+        {/* ✅ Image bien alignée à droite */}
+        <HeroImage src={Athlete} alt="Athlète en fauteuil roulant" />
+      </HeroContent>
     </HeroContainer>
   );
 };
