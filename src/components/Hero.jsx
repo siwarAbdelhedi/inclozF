296c41b82d261cb1e3454755c982d29833256f1c
Siwar Abdelhedi <siwar.abdelhedi@alten.com>
2025-04-24 12:05:31 +0200

    feat(hero): Adjustment styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,23 @@
 import { Box, Typography, Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Athlete from "../assets/home.png";
+import PatternBg from "../assets/photo2.png";
+import { Link } from "react-router-dom";
 
 const HeroContainer = styled(Box)(() => ({
   backgroundColor: "#FD5C35",
+  backgroundImage: `url(${PatternBg})`,
+  backgroundRepeat: "repeat",
+  backgroundSize: "contain",
   minHeight: "100vh",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
   width: "100%",
-  overflow: "hidden",
-}));
-
-const HeroContent = styled(Box)(() => ({
-  width: "100%",
-  maxWidth: "1600px",
-  padding: "0 80px",
   display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
   flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+  paddingLeft: "5%",
+  paddingRight: "2%",
+  overflow: "hidden",
   "@media (max-width: 960px)": {
     flexDirection: "column",
     padding: "40px 20px",
@@ -27,36 +25,35 @@ const HeroContent = styled(Box)(() => ({
   },
 }));
 
-
 const TextContainer = styled(Box)(() => ({
   maxWidth: "600px",
   "@media (max-width: 960px)": {
     textAlign: "center",
+    marginBottom: "30px",
   },
 }));
 
 const HeroImage = styled("img")(() => ({
+  maxHeight: "450px", 
   width: "auto",
-  maxWidth: "500px",
-  height: "80vh",
   objectFit: "contain",
   "@media (max-width: 960px)": {
-    height: "auto",
     maxWidth: "80%",
-    marginTop: "20px",
+    height: "auto",
+    marginTop: "30px",
   },
 }));
 
-
 const StyledButton = styled(Button)(() => ({
   backgroundColor: "white",
-  color: "#232A45",
+  color: "#14235E",
   fontWeight: "bold",
   fontSize: "18px",
   textTransform: "none",
   padding: "12px 24px",
   borderRadius: "30px",
   boxShadow: "3px 3px 10px rgba(0,0,0,0.2)",
+  marginTop: "30px",
   "&:hover": {
     backgroundColor: "#FFE5CF",
   },
@@ -65,27 +62,26 @@ const StyledButton = styled(Button)(() => ({
 const Hero = () => {
   return (
     <HeroContainer>
-      <HeroContent>
-        {/* Texte à gauche */}
-        <TextContainer>
-          <Typography
-            variant="h2"
-            sx={{
-              fontWeight: "bold",
-              color: "#14235E",
-              fontFamily: "Decalotype, sans-serif",
-              fontSize: { xs: "1.75rem", sm: "2.5rem", md: "3rem" },
-            }}
-          >
-            Votre marque de vêtements de sport <br />
-            <span style={{ color: "#14235E" }}>100% adaptable</span>
-          </Typography>
-          <StyledButton sx={{ mt: 3 }}>Qui sommes nous ?</StyledButton>
-        </TextContainer>
+      <TextContainer>
+        <Typography
+          variant="h2"
+          sx={{
+            fontWeight: "bold",
+            color: "#14235E",
+            fontFamily: "Decalotype, sans-serif",
+            fontSize: { xs: "1.75rem", sm: "2.5rem", md: "3rem" },
+          }}
+        >
+          Votre marque de vêtements de sport <br />
+          <span style={{ color: "#14235E" }}>100% adaptable</span>
+        </Typography>
 
-        {/* Image bien alignée à droite */}
-        <HeroImage src={Athlete} alt="Athlète en fauteuil roulant" />
-      </HeroContent>
+        <StyledButton component={Link} to="/about">
+          Qui sommes nous ?
+        </StyledButton>
+      </TextContainer>
+
+      <HeroImage src={Athlete} alt="Athlète en fauteuil roulant" />
     </HeroContainer>
   );
 };
